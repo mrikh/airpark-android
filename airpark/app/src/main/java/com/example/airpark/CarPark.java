cae033175fe2b31dcf3dbb1024d1cc4a05a7e071
Sinead-3 <60734340+Sinead-3@users.noreply.github.com>
2020-11-08 23:02:29 +0000

    Carpark, Carpark space interface and disabled space classes
    Initial car park class methods and carpark space interface methods, as well as the disabled space that implements it.

---
+++ airpark/app/src/main/java/com/example/airpark/CarPark.java
@@ -0,0 +1,31 @@
+public class CarPark {
+
+    private int carparkID;
+    private String carparkName, carparkType;
+    private double distanceFromAirport;
+
+    public CarPark(int carparkID, String carparkType, String carparkName, double distanceFromAirport){
+        this.carparkID = carparkID;
+        this.carparkName = carparkName;
+        this.carparkType = carparkType;
+        this.distanceFromAirport = distanceFromAirport;
+    }
+
+    public int getCarparkID(){ return carparkID; }
+
+    public  String getCarparkName(){ return carparkName; }
+
+    public  String getCarparkType(){ return carparkType; }
+
+    public double getDistanceFromAirport(){ return distanceFromAirport; }
+
+    public boolean isFull(int availableSpaces){
+        if(availableSpaces > 0){
+            return false;
+        }
+        return true;
+    }
+
+    public String toString(){
+        return carparkName + "\n" + carparkType + "\n\n" + distanceFromAirport + "m from Airport";}
+}
