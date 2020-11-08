6a3de1bff0216816b6b4fce966938a8699b7ca7f
Sinead-3 <60734340+Sinead-3@users.noreply.github.com>
2020-11-08 22:49:33 +0000

    Create Airport.java

---
+++ airpark/app/src/main/java/com/example/airpark/Airport.java
@@ -0,0 +1,15 @@
+package com.example.airparkapp;
+
+public class Airport {
+    private String airportName;
+    private int airportID;
+
+    public Airport(String name, int id){
+        this.airportName = name;
+        this.airportID = id;
+    }
+
+    public String getAirportName(){ return airportName; }
+
+    public int getAirportID(){ return airportID; }
+}
