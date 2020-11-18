fd72a8ebca6b1109fc804af7953ee874240e2fb5
Mayank Rikh <19018797@studentmail.ul.ie>
2020-11-18 01:34:04 +0000

    Added unit test

---
+++ airpark/app/src/test/java/com/example/airpark/InputValidatorTest.java
@@ -0,0 +1,37 @@
+package com.example.airpark;
+
+import com.example.airpark.utils.InputValidator;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class InputValidatorTest {
+
+    private InputValidator validator;
+
+    @BeforeEach
+    void setUp(){
+        validator = new InputValidator();
+    }
+
+    @AfterEach
+    void tearDown(){
+        validator = null;
+    }
+
+    @ParameterizedTest
+    @CsvSource({" , false", "'', false", "' ', false", "mayank, false", "mayankrikh@, false", "@gmail.com, false" , "mayankrikh@gmail, false", "mayankrikh@gmail.com, true", "mayankrikh@gmail.co.in, true"})
+    public void emailValidationTest(String email, boolean result) {
+        assertEquals(result, validator.isValidEmail(email));
+    }
+}
