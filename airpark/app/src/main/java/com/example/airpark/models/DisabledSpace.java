21daea210ab86714e1689aa43fb6e3fa17f72c7a
Sinead-3 <60734340+Sinead-3@users.noreply.github.com>
2020-12-03 18:56:02 +0000

    Fixed errors

--- airpark/app/src/main/java/com/example/airpark/models/DisabledSpace.java
+++ airpark/app/src/main/java/com/example/airpark/models/DisabledSpace.java
@@ -2,7 +2,7 @@ package com.example.airpark.models;
 
 public class DisabledSpace extends CarParkSpace {
 
-    DisabledSpace(int spaceID, int availableSpaces, String vehicleReg){
-        super(spaceID, availableSpaces,vehicleReg);
+    DisabledSpace(int spaceID, int availableSpaces, Vehicle vehicle){
+        super(spaceID, availableSpaces,vehicle);
     }
 }
--- airpark/app/src/main/java/com/example/airpark/models/GeneralSpace.java
+++ airpark/app/src/main/java/com/example/airpark/models/GeneralSpace.java
@@ -2,7 +2,7 @@ package com.example.airpark.models;
 
 public class GeneralSpace extends CarParkSpace{
 
-    GeneralSpace(int spaceID, int availableSpaces, String vehicleReg) {
-        super(spaceID, availableSpaces, vehicleReg);
+    GeneralSpace(int spaceID, int availableSpaces, Vehicle vehicle) {
+        super(spaceID, availableSpaces, vehicle);
     }
 }
