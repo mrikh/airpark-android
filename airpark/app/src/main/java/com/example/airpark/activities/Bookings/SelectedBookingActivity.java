bf3b44fea6f5033f15667751afb6e46dccadb5ed
Sinead-3 <60734340+Sinead-3@users.noreply.github.com>
2020-12-08 22:33:42 +0000

    Removed unneccessary text from SelectedBookingActivity and added 'back button'

--- airpark/app/src/main/java/com/example/airpark/activities/Bookings/SelectedBookingActivity.java
+++ airpark/app/src/main/java/com/example/airpark/activities/Bookings/SelectedBookingActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -69,7 +70,7 @@ public class SelectedBookingActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.carpark_image);
 
         Glide.with(this).load(booking.getCarparkImage()).into(imageView);
-        priceMoreInfo.setText(R.string.discount_info);
+        priceMoreInfo.setVisibility(View.GONE);
 
         // QR code button
         qrBtn.setOnClickListener(v -> {
