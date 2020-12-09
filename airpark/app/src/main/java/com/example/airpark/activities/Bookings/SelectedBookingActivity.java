3688a30ce781df048add3427741c48984b571098
rachelhunt <rachelhunt1998@gmail.com>
2020-12-09 10:26:56 +0000

    Writing delete button functionality with Observer Pattern

--- airpark/app/src/main/java/com/example/airpark/activities/Bookings/SelectedBookingActivity.java
+++ airpark/app/src/main/java/com/example/airpark/activities/Bookings/SelectedBookingActivity.java
@@ -3,26 +3,22 @@ package com.example.airpark.activities.Bookings;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.bumptech.glide.Glide;
 import com.example.airpark.R;
-import com.example.airpark.activities.EnterDetailsActivity;
 import com.example.airpark.activities.QRgeneratorActivity;
 import com.example.airpark.models.BookingModel;
-import com.example.airpark.models.BookingTicket;
-import com.example.airpark.models.CarPark;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-
-// Note: Code for QR code button and Delete button still to be fixed!
 
 public class SelectedBookingActivity extends AppCompatActivity {
 
@@ -66,7 +62,7 @@ public class SelectedBookingActivity extends AppCompatActivity {
         Glide.with(this).load(booking.getCarparkImage()).into(imageView);
         priceMoreInfo.setText(R.string.discount_info);
 
-        // QR code button -- ADD
+        // QR code button
         qrBtn.setOnClickListener(v -> {
             Intent intent = new Intent(this, QRgeneratorActivity.class);
             intent.putExtra("screen name", "my booking");
@@ -74,8 +70,14 @@ public class SelectedBookingActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        // change method for delete button -- ADD
-        deleteBtn.setOnClickListener(v -> {});
+        //     Observer Pattern - - Delete Button (Publisher)
+        // Every time the delete button is clicked, we want to broadcast a notification.
+        deleteBtn.setOnClickListener(v -> {findViewById(R.id.deleteButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }});
+        });
     }
 
     @Override
@@ -101,5 +103,24 @@ public class SelectedBookingActivity extends AppCompatActivity {
         deleteBtn = (ImageView)findViewById(R.id.deleteButton);
         priceMoreInfo = findViewById(R.id.more_price_info);
     }
+
+
+    //     Observer Pattern - deletion of listing from Bookings Page
+
+    // Publisher Activity Code (Sender) - - Details (where delete button is)
+
+    // Send an Intent with an action named "delete_booking". The Intent sent should
+    // be received by the ReceiverActivity in MyBookingsActivity.
+    private void sendMessage() {
+        Log.d("sender", "Broadcasting message");
+        Intent intent = new Intent("delete_booking");
+        // You can also include some extra data.
+        intent.putExtra("message", "This booking has now been deleted.");
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    // Note: every time the button @+id/deleteButton is clicked, an Intent is broadcasted
+    //  and is received by mMessageReceiver in ReceiverActivity (in MyBookingsActivity).
+
 
 }
